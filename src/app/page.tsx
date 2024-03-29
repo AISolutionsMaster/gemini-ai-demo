a1fc8aa8e38198c63351e3afa4c064b13349186e
thaiba <thaiba@aisolutionsjsc.com>
2024-03-29 17:49:22 +0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 
 import { Chatbot } from "@/components/Chatbot";
-import Generate from "@/components/generate";
-
 export default function Home() {
   return (
     <main className="h-screen flex  flex-col items-center  p-14">
